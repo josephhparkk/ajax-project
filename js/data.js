ff2063a4cdd163bf4aa87abc45b71fa0237291d7
Joseph Park <parkjoseph54@gmail.com>
2022-11-15 07:07:37 +0000

    in progress f#1

--- js/data.js
+++ js/data.js
@@ -1,1 +1,29 @@
 /* exported data */
+var data = {
+  view: 'search-page',
+  shows: [],
+  nextEntryId: 1
+};
+
+var previousDataJSON = localStorage.getItem('form');
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
+
+window.addEventListener('beforeunload', function () {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('form', dataJSON);
+});
+
+function getShowId(title) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.watchmode.com/v1/search/?apiKey=QE6cb8xjAt1kyYahavgtImCXbtkO5RVOIRNCP3Or&search_field=name&types=tv&search_value=breaking');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    // console.log(xhr.status);
+    // console.log(xhr.response);
+  });
+  xhr.send();
+}
+
+getShowId('love is blind');
